8ef6ace1243c14453cdf113a406634ff2ac78e79
Jorge Luis de la Torre <jorgedlt@users.noreply.github.com>
2016-01-31 17:16:47 -0500

    Update aws.mkssh-pub.sh

--- aws.mkssh-pub.sh
+++ aws.mkssh-pub.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-# Create Assible from AWS Enviroment
+# Create cut-&-paste jumpfile from AWS Enviroment
 
 INVfile=aws.PUB-jumpfile
 SSHkey='/home/ubuntu/.aws/m3DEVaws.pem'
 SSHuid='ubuntu'
 
-MASTER=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]' \
+MASTER=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,PublicIpAddress]' \
    --filter "Name=tag:Name,Values=AWSCLI v1.2 master" --output text | tr '\t' ' ')
 
-WORKERS=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]' --filter "Name=tag:Project,Value
+WORKERS=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,PublicIpAddress]' --filter "Name=tag:Project,Value
 s=HP-DEMO" "Name=instance-state-name,Values=running" --output text | tr '\t' ' ')
 
 echo '[Master]' > $INVfile
- while IFS=' ' read instanceID PrivateIpAddr; do
-     echo "ssh -i ${SSHkey} ${SSHuid}@${PrivateIpAddr}"
+ while IFS=' ' read instanceID PublicIpAddress; do
+     echo "ssh -i ${SSHkey} ${SSHuid}@${PublicIpAddress}"
  done <<< "$MASTER" >> $INVfile ; echo >> $INVfile
 
 echo '[Workers]' >> $INVfile
- while IFS=' ' read instanceID PrivateIpAddr; do
-     echo "ssh -i ${SSHkey} ${SSHuid}@${PrivateIpAddr}"
+ while IFS=' ' read instanceID PublicIpAddress; do
+     echo "ssh -i ${SSHkey} ${SSHuid}@${PublicIpAddress}"
  done <<< "$WORKERS" >> $INVfile
 
 cat $INVfile | ccze -A
