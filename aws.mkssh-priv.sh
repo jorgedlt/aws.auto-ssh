869b109f2622fab649990a9df367f25318e31a69
Jorge Luis de la Torre <jorgedlt@users.noreply.github.com>
2016-01-31 17:08:40 -0500

    Create aws.mkssh-priv.sh

---
+++ aws.mkssh-priv.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Create Assible from AWS Enviroment
+
+INVfile=aws.ssh-jumpfile
+SSHkey='/home/ubuntu/.aws/m3DEVaws.pem'
+SSHuid='ubuntu'
+
+MASTER=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]' \
+   --filter "Name=tag:Name,Values=AWSCLI v1.2 master" --output text | tr '\t' ' ')
+
+WORKERS=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]' --filter "Name=tag:Project,Value
+s=HP-DEMO" "Name=instance-state-name,Values=running" --output text | tr '\t' ' ')
+
+echo '[Master]' > $INVfile
+ while IFS=' ' read instanceID PrivateIpAddr; do
+     echo "ssh -i ${SSHkey} ${SSHuid}@${PrivateIpAddr}"
+ done <<< "$MASTER" >> $INVfile ; echo >> $INVfile
+
+echo '[Workers]' >> $INVfile
+ while IFS=' ' read instanceID PrivateIpAddr; do
+     echo "ssh -i ${SSHkey} ${SSHuid}@${PrivateIpAddr}"
+ done <<< "$WORKERS" >> $INVfile
+
+cat $INVfile | ccze -A
+
+exit 0;
